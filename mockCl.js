dbe2946ebafd7297866b19f451c68a13d3cec8f2
Casey Hungler <casey@rcinnov.com>
2017-12-19 20:53:45 -0800

    Add testing framework

---
+++ mockCl.js
@@ -0,0 +1,23 @@
+const stack = [];
+
+const cl = console.log;
+
+const start = (options = {}) => {
+	console.log = (...args) => {
+		const message = args.join(' ');
+		stack.push(message);
+		if (options.passThrough !== false) {
+			cl(message);
+		}
+	}
+};
+
+const stop = () => {
+	console.log = cl
+}
+
+module.exports = {
+	start,
+	stop,
+	stack
+};
